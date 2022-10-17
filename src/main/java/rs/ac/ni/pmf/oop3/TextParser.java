ba15c637307d52a00955caee0548bf9a2c064a36
marko <ninja643@gmail.com>
2022-10-17 14:19:17 +0200

    Initial commit

---
+++ src/main/java/rs/ac/ni/pmf/oop3/TextParser.java
@@ -0,0 +1,8 @@
+package rs.ac.ni.pmf.oop3;
+
+import java.util.Set;
+
+public interface TextParser
+{
+	TextStatistics getTextStatistics();
+}
---
+++ src/main/java/rs/ac/ni/pmf/oop3/TextStatistics.java
@@ -0,0 +1,11 @@
+package rs.ac.ni.pmf.oop3;
+
+import lombok.Value;
+
+@Value
+public class TextStatistics
+{
+	int wordsCount;
+	int distinctWords;
+	int ignoredWords;
+}
